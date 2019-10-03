53b35a5dca6170c8d0228f739dc26b408f0b6cb5
mpeij <43198315+mpeij@users.noreply.github.com>
2019-10-03 13:45:16 +0200

    Add files via upload

---
+++ coding/script.js
@@ -0,0 +1,11 @@
+function homeButtonClicked(){
+    window.open("./website.html", "_top");
+}
+
+function page1Clicked(){
+    window.open("./page 1.html", "_top");
+}
+
+function page2Clicked(){
+    window.open("./page 2.html", "_top");
+}
