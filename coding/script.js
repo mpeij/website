7ea69b15e9c620f1553fb7b6f72b0cd97718315c
mpeij <43198315+mpeij@users.noreply.github.com>
2019-11-13 22:21:53 +0100

    Add files via upload

---
+++ coding/script.js
@@ -0,0 +1,180 @@
+function homeButtonClicked(){
+    window.open("./home.html", "_top");
+}
+
+function allCountriesClicked(){
+    window.open("./allCountries.html", "_top");
+}
+
+function weatherPreferencesClicked(){
+    window.open("./weatherPreferences.html", "_top");
+}
+
+function climateClicked(){
+    window.open("./climate.html", "_top");
+}
+
+function generalTipsClicked(){
+    window.open("./tips.html", "_top");
+}
+
+function monthsClicked(){
+    window.open("./months.html", "_top");
+}
+
+function loginClicked(){
+    window.open("./login.html", "_top");
+}
+
+function favoriteClicked(){
+    window.open("./success.html", "_top");
+}
+
+
+
+
+function netherlandsClimateClicked(){
+    window.open("./netherlandsClimate.html", "_top");
+}
+
+function germanyClimateClicked(){
+    window.open("./germanyClimate.html", "_top");
+}
+
+function belgiumClimateClicked(){
+    window.open("./belgiumClimate.html", "_top");
+}
+
+function franceClimateClicked(){
+    window.open("./franceClimate.html", "_top");
+}
+
+function spainClimateClicked(){
+    window.open("./spainClimate.html", "_top");
+}
+
+function italyClimateClicked(){
+    window.open("./italyClimate.html", "_top");
+}
+
+function greeceClimateClicked(){
+    window.open("./greeceClimate.html", "_top");
+}
+
+function switzerlandClimateClicked(){
+    window.open("./zwitzerlandClimate.html", "_top");
+}
+
+function egyptClimateClicked(){
+    window.open("./egyptClimate.html", "_top");
+}
+
+function arabiaClimateClicked(){
+    window.open("./arabiaClimate.html", "_top");
+}
+
+function maledivesClimateClicked(){
+    window.open("./maledivesClimate.html", "_top");
+}
+
+function cubaClimateClicked(){
+    window.open("./cubaClimate.html", "_top");
+}
+
+function mexicoClimateClicked(){
+    window.open("./mexicoClimate.html", "_top");
+}
+
+function canadaClimateClicked(){
+    window.open("./canadaClimate.html", "_top");
+}
+
+function russiaClimateClicked(){
+    window.open("./russiaClimate.html", "_top");
+}
+
+
+
+
+function netherlandsClicked(){
+    window.open("./netherlands.html", "_top");
+}
+
+function germanyClicked(){
+    window.open("./germany.html", "_top");
+}
+
+function belgiumClicked(){
+    window.open("./belgium.html", "_top");
+}
+
+function franceClimate(){
+    window.open("./france.html", "_top");
+}
+
+function spainClicked(){
+    window.open("./spain.html", "_top");
+}
+
+function italyClicked(){
+    window.open("./italy.html", "_top");
+}
+
+function greeceClicked(){
+    window.open("./greece.html", "_top");
+}
+
+function switzerlandClicked(){
+    window.open("./switzerland.html", "_top");
+}
+
+function egyptClicked(){
+    window.open("./egypt.html", "_top");
+}
+
+function arabiaClicked(){
+    window.open("./arabia.html", "_top");
+}
+
+function maledivesClicked(){
+    window.open("./maledives.html", "_top");
+}
+
+function cubaClicked(){
+    window.open("./cuba.html", "_top");
+}
+
+function mexicoClicked(){
+    window.open("./mexico.html", "_top");
+}
+
+function canadaClicked(){
+    window.open("./canada.html", "_top");
+}
+
+function russiaClicked(){
+    window.open("./russia.html", "_top");
+}
+
+
+
+
+function januaryClicked(){
+    window.open("./january.html", "_top");
+}
+
+function februaryClicked(){
+    window.open("./february.html", "_top");
+}
+
+function marchClicked(){
+    window.open("./march.html", "_top");
+}
+
+function aprilClicked(){
+    window.open("./april.html", "_top");
+}
+
+function mayClicked(){
+    window.open("./may.html", "_top");
+}
